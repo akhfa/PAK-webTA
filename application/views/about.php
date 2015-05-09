37eef96036f4f4391b95d922eb5b65c1d5ec8257
akhfa <akhmadfakhoni@gmail.com>
2015-05-09 19:08:57 +0700

    change about footer

--- application/views/about.php
+++ application/views/about.php
@@ -79,40 +79,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
  <div class="clear"></div>
  </div>
 </div>
-<div class="f_bg">
-<div class="wrap">
-<div class="footer">
-		<div class="f_logo">
-			<a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>images/logo.png" alt=""></a>
-		<div class="copy">
-			<p class="w3-link">© All Rights Reserved | Design by&nbsp; <a href="<?php echo base_url()?>http://w3layouts.com/"> W3Layouts</a></p>
- 		</div>
- 		</div>
-		<div class="f_grid">
-		<div class="social">
-				<ul class="follow_icon">
-						<li><a href="<?php echo base_url()?>#" style="opacity: 1;">Get Updates Via</a></li>
-					<li><a href="<?php echo base_url()?>#" style="opacity: 1;"><img src="<?php echo base_url()?>images/fb.png" alt=""></a></li>
-					<li><a href="<?php echo base_url()?>#" style="opacity: 1;"><img src="<?php echo base_url()?>images/g+.png" alt=""></a></li>
-					<li><a href="<?php echo base_url()?>#" style="opacity: 1;"><img src="<?php echo base_url()?>images/tw.png" alt=""></a></li>
-					<li><a href="<?php echo base_url()?>#" style="opacity: 1;"><img src="<?php echo base_url()?>images/rss.png" alt=""></a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="f_grid1">
-			<div class="f_icon">
-				<img src="<?php echo base_url()?>images/f_icon.png" alt="" />
-			</div>
-			<div class="f_address">
-				<p>500 Lorem Ipsum Dolor Sit,</p>
-				<p>22-56-323 Lorem Ipsum Dolor Sit Sit Amet,</p>
-				<p>Fax: (000) 000 00 00 0</p>
-				<p>Email: <span>info@mycompany.com</span></p>
-		  </div>
-		</div>
-		<div class="clear"></div>
-</div>
-</div>
-</div>
+<?php require_once 'footer.php' ?>
 </body>
 </html>
