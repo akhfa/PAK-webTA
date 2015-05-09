5a111a0007015f62bf7c2e46589145c97f7a0b47
akhfa <akhmadfakhoni@gmail.com>
2015-05-09 20:03:19 +0700

    added list search

---
+++ application/controllers/Listpaper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akhfa
+ * Date: 09/05/15
+ * Time: 19:38
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Listpaper extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->load->view('list');
+    }
+}
---
+++ application/views/list.php
@@ -0,0 +1,52 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<?php require_once 'header.php' ?>
+<body>
+<div class="h-bg">
+    <div class="wrap">
+        <div class="header">
+            <div class="logo">
+                <a href="<?php echo base_url() ?>"><img src="<?php echo base_url()?>images/logo.png"> </a>
+            </div>
+            <div class="header-right">
+                <ul class="nav">
+                    <li><a href="<?php echo base_url()?>" title="Home">Home</a></li>
+                    <li><a href="<?php echo base_url() ?>index.php/about">Tentang Kami</a></li>
+                    <li class="active"><a href="<?php echo base_url() ?>index.php/listpaper">Daftar Paper</a></li>
+                    <li><a href="<?php echo base_url() ?>index.php/contact">Kontak</a></li>
+                    <li><a href="<?php echo base_url() ?>index.php/login">Login</a></li>
+                </ul>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<div class="content_bg">
+    <div class="wrap">
+        <div class="content">
+            <div class="main">
+<!--                Tabel list dari TA nya-->
+
+            </div>
+            <div class="sidebar">
+                <div class="side_bar">
+                    <h2>Catogories</h2>
+                    <p class="top"><a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>images/art-pic1.jpg" alt="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip consequat.</a></p>
+                    <p class="top"><a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>images/art-pic2.jpg" alt="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip consequat.</a></p>
+                    <p class="top"><a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>images/art-pic3.jpg" alt="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip consequat.</a></p>
+                    <p class="top"><a href="<?php echo base_url()?>"><img src="<?php echo base_url()?>images/art-pic4.jpg" alt="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip consequat.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<?php require_once 'footer.php' ?>
+</body>
+</html>
